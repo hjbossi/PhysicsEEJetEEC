3b7541b412896573aa4407d8ae0ba06c55c09bac
Hannah <hannah.bossi@cern.ch>
2025-02-22 17:11:51 +0100

    [syst]: Add in the new binnning systematic.

---
+++ Systematics/20250219_BinningSystematic/plotBinning.sh
@@ -0,0 +1,8 @@
+echo "-------> Starting to plot"
+./ExecutePlotBinningSyst \
+        --Output BinningSystV3WithData \
+        --Label "Data 1D","Data 2D and Projected"\
+        --Prefix "02192025" \
+        --DoRatio true \
+        --UseData true \
+        --DoWeight false
